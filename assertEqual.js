dd70156af9c7de143278c4316522fad9c5a4bfbd
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-04-04 20:13:02 +0000

    created custom assert equals function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 3);
