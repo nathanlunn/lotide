1c10b35ece8543542437b7226fd9b2cc1582690f
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-04-04 22:09:45 +0000

    added a tail function to tail javascript file

---
+++ tail.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function tail(array) {
+  return array.slice(1);
+};
+
+const words = [];
+assertEqual(tail(words).length, 0);
