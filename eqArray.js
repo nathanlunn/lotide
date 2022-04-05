f1b78f6768fcdbc9af525f246bce4e083f0d7be7
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-04-05 18:32:35 +0000

    added eqArray.js

---
+++ eqArray.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays  = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
