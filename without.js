7370ef004ff329e24628b4f715aa7efb1b47c176
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-04-05 19:08:09 +0000

    added without.js

---
+++ without.js
@@ -0,0 +1,31 @@
+const eqArrays  = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArrayEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const without = function(original, deletions) {
+  let result = [];
+  for (let el of original) {
+    if (!deletions.includes(el)) {
+      result.push(el);
+    }
+  }
+  return result;
+};
