b275b4e5932be5ffc78da14eba6d5be22a22e90a
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-04-06 00:17:38 +0000

    added letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,42 @@
+const eqArrays  = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArrayEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  
+  return results;
+};
+
+assertArrayEqual(letterPositions("hello")["h"], [0]);
+assertArrayEqual(letterPositions("hello")["e"], [1]);
+assertArrayEqual(letterPositions("hello")["l"], [2, 3]);
+assertArrayEqual(letterPositions("hello")["o"], [4]);
