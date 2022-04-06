30512c1e707c72c3d03d100b6592191fc9927f83
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-04-06 01:29:28 +0000

    corrected name on assertArraysEquals.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays  = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
