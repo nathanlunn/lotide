3d337c461b5849b0f1eeb30c8ede0e655faaba6b
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-04-06 23:39:57 +0000

    removed unnecessary else's from middle.js

--- middle.js
+++ middle.js
@@ -25,13 +25,13 @@ const middle = function(array) {
   let result = [];
   if (array.length < 3) {
     return result;
-  } else if (array.length % 2 === 0) {
+  }
+  if (array.length % 2 === 0) {
     result.push(array[array.length / 2 - 1], array[array.length / 2]);
     return result;
-  } else {
+  }
     result.push(array[Math.floor(array.length / 2)]);
     return result;
-  }
 };
 
-assertArrayEqual(middle([1, 2, 3, 5, 2, 6, 7, 5, 6, 7]), [2, 6]);
+assertArrayEqual(middle([1,5,6,7,8,8]), [6,7]);
