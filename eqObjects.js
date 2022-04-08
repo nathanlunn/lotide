f3d4a3e14650aac3fc43cbc8f867715634230861
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-04-08 22:27:14 +0000

    eqObjects.js now checks for objects within objects via recursion

--- eqObjects.js
+++ eqObjects.js
@@ -33,6 +33,8 @@ const eqObjects = function(object1, object2) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
+    } else if (typeof object1[key] === "object") {
+      return eqObjects(object1[key], object2[key]);
     } else if (object1[key] !== object2[key]) {
       return false;
     }
@@ -48,5 +50,11 @@ const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
 // eqObjects(cd, cd2); // => false
 
-assertEqual(eqObjects(cd, dc), true);
-assertEqual(eqObjects(cd, cd2), false);
+// assertEqual(eqObjects(cd, dc), true);
+// assertEqual(eqObjects(cd, cd2), false);
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
+
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: { h: 0, j: {l: 0, u:}} }, { a: { y: 0, z: 1 }, b: 2 }), true)
