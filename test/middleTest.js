c389b4553f7d10a99e590f281d0a1f334610e9ef
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-04-11 13:58:33 +0000

    refactored mocha tests for tail and middle to use deepEquals

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,17 +1,16 @@
 const middle = require('../middle');
-const eqArrays = require('../eqArray');
 const assert = require('chai').assert;
 
 describe('middle', () => {
   it('should return [3] when passed [1, 2, 3, 4, 5]', () => {
-    assert.strictEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
 
   it('should return [3, 4] when passed [5, 7, 3, 4, 9, 10]', () => {
-    assert.strictEqual(eqArrays(middle([5, 7, 3, 4, 9, 10]), [3, 4]), true);
+    assert.deepEqual(middle([5, 7, 3, 4, 9, 10]), [3, 4]);
   });
   
   it('should return an empty array when passed [1, 2]', () => {
-    assert.strictEqual(eqArrays(middle([1, 2]), []), true);
+    assert.deepEqual(middle([1, 2]), []);
   });
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,14 @@
 const tail = require('../tail');
-const eqArrays = require('../eqArray');
 const assert = require('chai').assert;
 
 
 describe('tail', () => {
   it('should return [2, 3, 4] when passed [1, 2, 3, 4]', () => {
-    assert.strictEqual(eqArrays(tail([1, 2, 3, 4]), [2, 3, 4]), true);
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
   });
 
   it('should return an empty array if passed an array with 1 element', () => {
-    assert.strictEqual(eqArrays(tail([1]), []), true);
+    assert.deepEqual(tail([1]), []);
   });
 });
 
