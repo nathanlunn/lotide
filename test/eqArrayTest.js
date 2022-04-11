3f07402401d9963c8a4eab9680b5a46d9a1eedd9
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-04-11 13:54:59 +0000

    added mocha testing to eqArray.js

--- test/eqArrayTest.js
+++ test/eqArrayTest.js
@@ -1,5 +1,12 @@
-const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArray');
+const assert = require('chai').assert;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
+describe('eqArrays', () => {
+  it('should return true when passed [1, 2, 3] and [1, 2, 3]', () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it('should return false when passed [1, 2, 3] and [1, 3, 5]', () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 3, 5]), false);
+  });
+});
